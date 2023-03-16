9ff6dd639012ba7618061fb563e4bbfad16882e5
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-03-16 01:55:06 +0000

    feat: setup sock
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+import makeWASocket, { useMultiFileAuthState, DisconnectReason } from '@gampang-pkg/baileys-edge';
+import {type Boom} from '@hapi/boom';
+
+export const runImpl = async (sock?: ReturnType<typeof makeWASocket>): Promise<void> => {
+    const auth = await useMultiFileAuthState('sessions');
+
+    sock = makeWASocket({
+        auth: auth.state,
+        printQRInTerminal: true,
+    });
+
+    sock.ev.on('connection.update', async (conn) => {
+        const error = conn.lastDisconnect?.error as Boom;
+        
+        if (error && error.output.statusCode !== DisconnectReason.loggedOut && conn.connection === 'close') {
+            sock?.ws.close();
+            runImpl(sock);
+        } else if (error && error.output.statusCode === DisconnectReason.loggedOut) {
+            console.log('Logged out');
+            process.exit(0);
+        }
+        await auth.saveCreds();
+    });
+}
+
+runImpl();
